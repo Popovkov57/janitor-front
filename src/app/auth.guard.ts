b1c894226188600d164dc5fe2dfe0b9d5246ab35
Popovkov57 <kevin.patrzek@live.fr>
2022-11-02 17:31:04 +0100

    Update auth service and add auth interceptor and guard

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    private toastr: ToastrService
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      
+    if (this.authService.isLoggedIn !== true) {
+      this.toastr.error("Vous n'êtes pas authentifié !", "Erreur");
+      this.router.navigate(['login']);
+    }
+
+    return true;
+  }
+  
+}
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './user';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,8 @@ import { Observable } from 'rxjs';
 export class AuthService {
   
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
     ) { }
   
   signup(newUser: User): Observable<any> {
@@ -20,4 +22,19 @@ export class AuthService {
     return this.http.post('/api/auth/signin', user)
   }
 
+  getToken() {
+    return localStorage.getItem('accessToken');
+  }
+
+  get isLoggedIn(): boolean {
+    let accessToken = localStorage.getItem('accessToken');
+    return accessToken != null ? true : false;  
+  }
+
+  logout() {
+    let accessToken = localStorage.removeItem('accessToken');
+    if (accessToken == null) {
+      this.router.navigate(['login']);
+    }
+  }
 }
